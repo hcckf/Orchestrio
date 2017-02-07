c003306ed3ebf8d1a0c74b15d47a654c1a12de4a
Nate Krofft <Nate Krofft>
2017-02-07 16:46:27 -0600

    adding files

--- readme.md
+++ readme.md
@@ -2,10 +2,14 @@
  - Install node.js (https://nodejs.org/en/)
  - Install yarn (https://yarnpkg.com/docs/install)
 
- To start the server run the following commands in the code directory:
+The following commands only need to be run ONCE:
+`yarn install`
 
+ To start the server run the following commands in the code directory:
 ```
  node_modules/.bin/webpack -p
 
  node_modules/.bin/babel-node --presets 'react,es2015' app/server.js
 ```
+
+Visit localhost:8080 in your browser!
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,24 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: ['webpack-hot-middleware/client', path.join(__dirname, 'app', 'app.js')],
+  entry: path.join(__dirname, 'app', 'app.js'),
   output: {
     path: path.join(__dirname, 'app', 'static', 'js'),
-    filename: 'bundle.js',
-    publicPath: path.join(__dirname, 'app', 'static', 'js')
+    filename: 'bundle.js'
   },
   module: {
     loaders: [{
       test: path.join(__dirname, 'app'),
-      loader: ['babel-loader?' + JSON.stringify({
+      loader: ['babel-loader?'+JSON.stringify({
         cacheDirectory: 'babel_cache',
-        presets: ['react', 'es2015', 'react-hmre']
-      })]
+        presets: ['react', 'es2015']
+      })],
     }]
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
