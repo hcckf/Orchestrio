70709216d06b4652203cff05d9a32a228f5793a8
hcckf <henrystl@yahoo.com>
2017-02-20 14:56:01 -0600

    test

--- app/components/HelloWorld.js
+++ app/components/HelloWorld.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class HelloWorld extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
+		<div>
+			<h1>Hello World</h1>
+		</div>
     );
   }
 }
