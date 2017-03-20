658a29f09b068806aceb80f18e6c1f475c069418
hcckf <henrystl@yahoo.com>
2017-03-20 13:29:28 -0500

    Add files via upload

--- app/components/HelloWorld.js
+++ app/components/HelloWorld.js
@@ -3,9 +3,7 @@ import React from 'react';
 export default class HelloWorld extends React.Component {
   render() {
     return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+      <h1>Hello World</h1>
     );
   }
 }
